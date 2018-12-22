ebc2c90efd9d628c988bbf3976789196bd1d8a0b
Alexander Potts <alexander.potts@gmail.com>
2018-12-22 14:28:57 +0000

    Added error handling to run.py

--- run.py
+++ run.py
@@ -1,15 +1,21 @@
 """
-Entrypoint for program
+Entry-point for program
 """
+
+from os import environ
 
 from namecheap.updater import Updater
 
 if __name__ == '__main__':
-    domain_file = '/usr/src/app/domains.txt'
-    with open(domain_file) as file:
-        domains = [line.strip() for line in file]
-    up = Updater(domains)
     try:
+        domain_file = environ['domain_file']
+        with open(domain_file) as file:
+            domains = [line.strip() for line in file]
+        up = Updater(domains)
         up.run()
+    except KeyError as e:
+        print(f'env vars not set: {e}')
     except AttributeError as e:
         print(e)
+    except KeyboardInterrupt:
+        print('\nQuitting')
